5acfb37efdb0a46cffaa62cfc3b9e828356cab25
Paulo Tebet <tebetpaulo91@yahoo.com>
2025-03-13 19:40:45 -0300

    feach projeto colecoes

--- colecoes/src/control/Programa.java
+++ colecoes/src/control/Programa.java
@@ -1,4 +1,16 @@
 package control;
 
+import model.Empregado;
+import model.Empresa;
+
 public class Programa {
+
+    public static void main(String[] args){
+        Empresa empresa = new Empresa();
+
+        Empregado empregado1 = new Empregado();
+
+        Empregado empregado2 = new Empregado();
+
+    }
 }
--- colecoes/src/model/Empresa.java
+++ colecoes/src/model/Empresa.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empresa {
+
+    private List<Empregado> empregados = new ArrayList<>();
+
+    public void addEmpregado(Empregado empregado){
+        this.empregados.add(empregado);
+    }
+
+    public void removeEmpregado(Empregado empregado){
+        this.empregados.add(empregado);
+    }
 }
